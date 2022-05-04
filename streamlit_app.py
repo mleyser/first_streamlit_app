dcd44abf90271f0a3651ae21a6c3de72655130b2
Mahapan Leyser <104196346+mleyser@users.noreply.github.com>
2022-05-04 10:27:46 +0200

    Update streamlit_app.py
    add some functions and orders to interact with snowflake

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,3 +35,10 @@ streamlit.dataframe(fruityvice_normalized)
 
 
 import snowflake.connector
+
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT_(), CURRENT_REGION()")
+my_data_row = my_cur.fetchone()
+streamlit.text("Hello from Snowflake:")
+streamlit.text(my_data_row)
