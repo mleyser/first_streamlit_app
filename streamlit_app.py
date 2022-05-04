17fa213a0fe6b54ef5107c78716e2a27c8157bf3
Mahapan Leyser <104196346+mleyser@users.noreply.github.com>
2022-05-04 12:33:52 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,7 +40,12 @@ try:
     back_from_function = get_fruityvice_data(fruit_choice)
     streamlit.dataframe(back_from_function)
 
-
+# take the json version of the response and normalize it
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+#output it the screen as a table
+streamlit.dataframe(fruityvice_normalized)
+# do not run anything past here while we troubleshoot
+streamlit.stop()
 
 # snowflake connector
 
